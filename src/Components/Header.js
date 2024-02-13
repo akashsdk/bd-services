6b805e7cfeb6fa7922bd14b32751cb90a01d3345
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-02-13 22:55:08 +0600

    ok

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,6 +3,17 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <div className='Header-Body'>Header</div>
+    <div className='Header-Body'>
+      <div className='Header-Box1'>
+        Name and icon
+      </div>
+      <div className='Header-Box2'>
+        <p>Services</p>
+        <p>About Us</p>
+        <p>Contact</p>
+        <div>Lungue</div>
+      </div>
+      <div  className='Header-Box3'>profile</div>
+    </div>
   )
 }
