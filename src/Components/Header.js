5c02d69ae44eba667ad7ea3deb4a58f79d1f08b1
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-02-14 00:24:31 +0600

    ok

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,34 @@
-import React from 'react'
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+import Logo from "../Icon/Header logo2 .png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className='Header-Body'>
-      <div className='Header-Box1'>
-        Name and icon
+    <div className="Header-Body">
+      <div className="Header-Box1">
+        <Link to="/">
+          <img className="Header-Icon" src={Logo} alt="" />
+        </Link>
       </div>
-      <div className='Header-Box2'>
-        <p>Services</p>
-        <p>About Us</p>
-        <p>Contact</p>
+      <div className="Header-Box2">
+        <Link className="Header-Link">
+          <p className="Header-Text">Services</p>
+          
+        </Link>
+
+        <Link className="Header-Link">
+          <p className="Header-Text">About Us</p>
+        </Link>
+
+        <Link className="Header-Link">
+          <p className="Header-Text">Contact</p>
+        </Link>
+
         <div>Lungue</div>
       </div>
-      <div  className='Header-Box3'>profile</div>
+      <div className="Header-Box3">profile</div>
     </div>
-  )
+  );
 }
