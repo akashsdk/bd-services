700115ca0c0bf9677b0bbbeb3aa0e42f52a3a300
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-02-15 23:52:46 +0600

    ok

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./Header.css";
 
 import Logo from "../Icon/Header logo2 .png";
+import PImg from "../Image/Profile.png";
 import { Link } from "react-router-dom";
 import { Button, Drawer, Menu, Switch } from "antd";
 import {
   UpOutlined,
   DownOutlined,
   SettingOutlined,
-  AppstoreOutlined,
+  PicLeftOutlined,
 } from "@ant-design/icons";
 
 export default function Header() {
@@ -44,89 +45,115 @@ export default function Header() {
 
   return (
     <div className="Header-Body">
-      <div className="Header-Box1">
-        <Link to="/" onClick={() => handleTextClick(4)}>
-          <img className="Header-Logo" src={Logo} alt="" />
-        </Link>
-      </div>
+      {/* Header-Dextop */}
+      <div className="Header-Dextop">
+        <div className="Header-Box1">
+          <Link to="/" onClick={() => handleTextClick(4)}>
+            <img className="Header-Logo" src={Logo} alt="" />
+          </Link>
+        </div>
 
-      <div className="Header-Box2">
-        <div className="dropdown">
-          <p className={`Header-Text ${textColors[1]}`}>Services</p>
-          <DownOutlined className="Header-Icon1" />
-          <UpOutlined className="Header-Icon2" />
+        <div className="Header-Box2">
+          <div className="dropdown">
+            <p className={`Header-Text ${textColors[1]}`}>Services</p>
+            <DownOutlined className="Header-Icon1" />
+            <UpOutlined className="Header-Icon2" />
 
-          <div className="dropdown-content">
-            <Link
-              onClick={() => handleTextClick(1)}
-              to="/"
-              className="dropdown-content-Link"
-            >
-              <p className="dropdown-content-Text">Option 1</p>
-            </Link>
-            <Link
-              onClick={() => handleTextClick(1)}
-              to="/"
-              className="dropdown-content-Link"
-            >
-              <p className="dropdown-content-Text">Option 2</p>
-            </Link>
-            <Link
-              onClick={() => handleTextClick(1)}
-              to="/"
-              className="dropdown-content-Link"
-            >
-              <p className="dropdown-content-Text">Option 3</p>
-            </Link>
-            <Link
-              onClick={() => handleTextClick(1)}
-              to="/"
-              className="dropdown-content-Link"
-            >
-              <p className="dropdown-content-Text">Option 4</p>
-            </Link>
+            <div className="dropdown-content">
+              <Link
+                onClick={() => handleTextClick(1)}
+                to="/"
+                className="dropdown-content-Link"
+              >
+                <p className="dropdown-content-Text">Option 1</p>
+              </Link>
+              <Link
+                onClick={() => handleTextClick(1)}
+                to="/"
+                className="dropdown-content-Link"
+              >
+                <p className="dropdown-content-Text">Option 2</p>
+              </Link>
+              <Link
+                onClick={() => handleTextClick(1)}
+                to="/"
+                className="dropdown-content-Link"
+              >
+                <p className="dropdown-content-Text">Option 3</p>
+              </Link>
+              <Link
+                onClick={() => handleTextClick(1)}
+                to="/"
+                className="dropdown-content-Link"
+              >
+                <p className="dropdown-content-Text">Option 4</p>
+              </Link>
+            </div>
+          </div>
+
+          <Link
+            className="Header-Link"
+            to="/AboutUs"
+            onClick={() => handleTextClick(2)}
+          >
+            <p className={`Header-Text ${textColors[2]}`}>About Us</p>
+          </Link>
+
+          <Link
+            className="Header-Link"
+            to="/Contact"
+            onClick={() => handleTextClick(3)}
+          >
+            <p className={`Header-Text ${textColors[3]}`}>Contact</p>
+          </Link>
+
+          <div style={{ marginLeft: "20px" }}>
+            <Switch
+              checkedChildren="ENG"
+              unCheckedChildren="BN"
+              defaultChecked
+            />
           </div>
         </div>
 
-        <Link
-          className="Header-Link"
-          to="/AboutUs"
-          onClick={() => handleTextClick(2)}
-        >
-          <p className={`Header-Text ${textColors[2]}`}>About Us</p>
-        </Link>
+        <div className="Header-Box3">
+          <Link
+            className="Header-Box3-Link"
+            to="/Settings"
+            onClick={() => handleTextClick(0)}
+          >
+            <SettingOutlined className={`Header-Box3-Icon ${textColors[0]}`} />
+          </Link>
 
-        <Link
-          className="Header-Link"
-          to="/Contact"
-          onClick={() => handleTextClick(3)}
-        >
-          <p className={`Header-Text ${textColors[3]}`}>Contact</p>
-        </Link>
-
-        <div style={{ marginLeft: "20px" }}>
-          <Switch checkedChildren="ENG" unCheckedChildren="BN" defaultChecked />
+          <Link to="/Profile" onClick={() => handleTextClick(5)}>
+            <img className="Header-Box3-Img" alt="" src={PImg} />
+          </Link>
         </div>
       </div>
 
-      <div className="Header-Box3">
-        <Link
-          className="Header-Box3-Link"
-          to="/Settings"
-          onClick={() => handleTextClick(0)}
-        >
-          <SettingOutlined  className={`Header-Box3-Icon ${textColors[0]}`}/>
-        </Link>
-
-        <Link to="/Profile" onClick={() => handleTextClick(5)}>
-          <img
-            className="Header-Box3-Img"
-            alt=""
-            src="https://www.flagcolorcodes.com/data/flag-of-bangladesh.png"
-          />
-        </Link>
+      {/* Header-Mobile */}
+      <div className="Header-Mobile">
+        <div className="Header-M-Div">
+          <PicLeftOutlined onClick={showDrawer} className="Header-M-Icon"/>
+        </div>
+        <div className="Header-M-Div2">
+          <Link to="/" onClick={() => handleTextClick(4)}>
+            <img className="Header-Logo" src={Logo} alt="" />
+          </Link>
+        </div>
       </div>
-      <box></box>
+      <box>
+        <Drawer
+          placement="left"
+          title={<div>n</div>}
+          onClose={onClose}
+          open={open}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
+      </box>
     </div>
   );
 }
